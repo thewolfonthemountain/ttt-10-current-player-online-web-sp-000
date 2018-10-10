5ba48331d855287b5179d007703b73f853042bf1
Sean Langton <slangton1@gmail.com>
2018-10-10 02:15:58 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,8 @@
 def turn_count(board)
 counter = 0
-  board.each do |space|
-    if space == "X" || space == "O" then counter = counter + 1
+board.each do |space|
+    if space == "X" then counter += 1
     end
   end
+  puts counter
   end
